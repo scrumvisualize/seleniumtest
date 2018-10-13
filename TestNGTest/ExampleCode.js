f40b508951b39483cf80e787ed52164f063e7351
admin <scrumvisualize@gmail.com>
2018-10-13 14:18:12 +1300

    Selenium test first commit

---
+++ TestNGTest/ExampleCode.js
@@ -0,0 +1,7 @@
+function AddTwoNumbers(a,b) {
+  return a + b; //Function to return addition of two numbers a and b.
+}
+ 
+function MultiplyTwoNumbers(a,b) {
+	  return a * b; //Function to return product of two numbers a and b.
+}
